a36e85196c7f1aa4eb65f6ee3d57f3383221ce5a
Edward White <edw@rdjam.es>
2025-05-16 20:51:59 -0400

    debugged db error

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { require: true, rejectUnauthorized: false }
 });
 
 export async function saveUserTokens({ hubspot_user_id, access_token, refresh_token }) {
